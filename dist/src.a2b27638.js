8ea560876e397c0b579e7d1de38a1c6df2e88f6c
eriksachse <erik@napoleon.services>
2021-02-27 12:47:26 +0100

    123

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -35547,7 +35547,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65288" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65035" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
